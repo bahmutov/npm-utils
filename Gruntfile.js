1f7a03245e55d7a7b4764a5ace38d44cfad4bddc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-05 13:41:34 -0400

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+      },
+      'default': {
+        src: [ 'index.js', 'src/*.js' ]
+      }
+    }
+  });
+
+  var plugins = require('matchdep').filterDev('grunt-*');
+  plugins.forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('default', ['jshint']);
+};
