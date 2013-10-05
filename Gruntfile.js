ade7d58907b663e8f3f81416716a5bdd6110cec1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-05 13:56:00 -0400

    using grunt-complexity, refactored gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,15 @@ module.exports = function(grunt) {
       'default': {
         src: [ 'index.js', 'src/*.js' ]
       }
+    },
+
+    complexity: {
+      default: grunt.file.readJSON('complexity.json')
     }
   });
 
   var plugins = require('matchdep').filterDev('grunt-*');
   plugins.forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'complexity']);
 };
