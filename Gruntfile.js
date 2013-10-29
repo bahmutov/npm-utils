f627bda48c214b451f05b09e2913382e2d3d3e0f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-10-29 16:37:32 -0400

    added grunt-nice-package, fixes #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,16 @@ module.exports = function(grunt) {
     jshint: grunt.file.readJSON('jshint.json'),
     complexity: {
       default: grunt.file.readJSON('complexity.json')
+    },
+    'nice-package': {
+      all: {
+        options: {}
+      }
     }
   });
 
   var plugins = require('matchdep').filterDev('grunt-*');
   plugins.forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('default', ['jshint', 'complexity']);
+  grunt.registerTask('default', ['nice-package', 'jshint', 'complexity']);
 };
---
+++ History.md
@@ -0,0 +1,14 @@
+
+0.0.5 / 2013-10-29
+==================
+
+  * upgraded all out of date dependencies
+
+0.0.4 / 2013-10-29
+==================
+
+  * moved jshint settings from grunt to separate json file
+  * using grunt-complexity, refactored gruntfile
+  * added pre-git
+  * added grunt
+  * added repository url
