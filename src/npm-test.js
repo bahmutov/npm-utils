af018aa7835bfcf60f2dc2ed08d4bd108add277d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-08 10:15:39 -0500

    chore(complexity): refactored most complex source file

--- src/npm-test.js
+++ src/npm-test.js
@@ -4,6 +4,40 @@ var spawn = require('win-spawn');
 var q = require('q');
 var NPM_PATH = require('./npm-path');
 
+function testRunner(app, parts, stdoutLogger, stderrLogger) {
+  var npm = spawn(app, parts, { stdio: 'inherit' });
+  var testErrors = '';
+
+  npm.stdout.setEncoding('utf-8');
+  npm.stderr.setEncoding('utf-8');
+
+  npm.stdout.on('data', function (data) {
+    stdoutLogger(data);
+  });
+
+  npm.stderr.on('data', function (data) {
+    stderrLogger(data);
+    testErrors += data;
+  });
+
+  npm.on('error', function (err) {
+    console.error(err);
+    testErrors += err.toString();
+  });
+
+  var deferred = q.defer();
+  npm.on('exit', function (code) {
+    if (code) {
+      deferred.reject({
+        code: code,
+        errors: testErrors
+      });
+    }
+    deferred.resolve();
+  });
+  return deferred.promise;
+}
+
 // returns a promise
 function test(cmd, loggers) {
   var app = NPM_PATH;
@@ -30,39 +64,7 @@ function test(cmd, loggers) {
   la(check.unemptyString(app), 'application name should be a string', app);
   la(check.arrayOfStrings(parts), 'arguments should be an array', parts);
 
-  var npm = spawn(app, parts, { stdio: 'inherit' });
-  var testOutput = '';
-  var testErrors = '';
-
-  npm.stdout.setEncoding('utf-8');
-  npm.stderr.setEncoding('utf-8');
-
-  npm.stdout.on('data', function (data) {
-    stdoutLogger(data);
-    testOutput += data;
-  });
-
-  npm.stderr.on('data', function (data) {
-    stderrLogger(data);
-    testErrors += data;
-  });
-
-  npm.on('error', function (err) {
-    console.error(err);
-    testErrors += err.toString();
-  });
-
-  var deferred = q.defer();
-  npm.on('exit', function (code) {
-    if (code) {
-      deferred.reject({
-        code: code,
-        errors: testErrors
-      });
-    }
-    deferred.resolve();
-  });
-  return deferred.promise;
+  return testRunner(app, parts, stdoutLogger, stderrLogger);
 }
 
 module.exports = test;
