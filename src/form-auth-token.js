976e848fbe7dbd5f4b0c0d16bb66d533c7cedb8d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-26 13:06:42 -0400

    better error message on missing NPM_TOKEN

--- src/form-auth-token.js
+++ src/form-auth-token.js
@@ -2,11 +2,12 @@
 
 var la = require('lazy-ass');
 var is = require('check-more-types');
+var getNpmToken = 'https://www.npmjs.com/package/get-npm-token';
 
 function formAuthToken (registryUrl, tokenEnvName) {
   if (!tokenEnvName) {
     if (!process.env.NPM_TOKEN) {
-      throw new Error('Cannot find NPM_TOKEN');
+      throw new Error('Cannot find NPM_TOKEN\nuse ' + getNpmToken + ' to get one');
     }
     tokenEnvName = 'NPM_TOKEN';
   }
